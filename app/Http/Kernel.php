1d1a0833070be8248c613d12c798759e8922c117
Martin Collins <hellofromhell@gmail.com>
2015-03-03 19:23:21 +0000

    remove CSRF from kernal, should be temp

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel {
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
-		'FirstSite\Http\Middleware\VerifyCsrfToken',
+		// 'FirstSite\Http\Middleware\VerifyCsrfToken',
 	];
 
 	/**
